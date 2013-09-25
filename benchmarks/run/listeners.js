920c53864d983ebba605041ab796cbf38a268539
3rd-Eden <info@3rd-Eden.com>
2013-09-25 23:56:44 +0200

    [fix] Incorrect benchmark, returns original array instead of a clone

--- benchmarks/run/listeners.js
+++ benchmarks/run/listeners.js
@@ -37,9 +37,13 @@ ee2.setMaxListeners(Infinity);
 ).add('EventEmitter 1', function test1() {
   ee1.on('foo', handle);
   ee1.listeners('foo');
-}).add('EventEmitter 2', function test2() {
-  ee2.on('foo', handle);
-  ee2.listeners('foo');
+//
+// // EE2 doesn't correctly handle listeners as they can be removed by doing a
+// // ee2.listeners('foo').length = 0; kills the event emitter
+// }).add('EventEmitter 2', function test2() {
+//   ee2.on('foo', handle);
+//   ee2.listeners('foo');
+//
 }).add('EventEmitter 3', function test2() {
   ee3.on('foo', handle);
   ee3.listeners('foo');
