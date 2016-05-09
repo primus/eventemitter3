d66a4bc9187d57a0b972309e8f4b5f8ba47bb26b
Luigi Pinca <luigipinca@gmail.com>
2016-05-09 16:28:29 +0200

    [minor] Use a prebuilt constructor to get a "clean" empty object
    In some engines (e.g. V8) this is faster than calling
    `Object.create(null)`.

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 'use strict';
 
-var has = Object.prototype.hasOwnProperty;
+var has = Object.prototype.hasOwnProperty
+  , prefix = '~';
+
+/**
+ * This constructor is used to create a storage for our EE objects.
+ * An `Events` instance is a plain object whose properties are event names.
+ *
+ * @constructor
+ * @api private
+ */
+function Events() {}
 
 //
-// We store our EE objects in a plain object whose properties are event names.
+// We try to not inherit from `Object.prototype`. In some engines creating an
+// instance in this way is faster than calling `Object.create(null)` directly.
 // If `Object.create(null)` is not supported we prefix the event names with a
-// `~` to make sure that the built-in object properties are not overridden or
-// used as an attack vector.
-// We also assume that `Object.create(null)` is available when the event name
-// is an ES6 Symbol.
+// character to make sure that the built-in object properties are not
+// overridden or used as an attack vector.
 //
-var prefix = typeof Object.create !== 'function' ? '~' : false;
+if (Object.create) {
+  Events.prototype = Object.create(null);
+
+  //
+  // This hack is needed because the `__proto__` property is still inherited in
+  // some old browsers like Android 4, iPhone 5.1, Opera 11 and Safari 5.
+  //
+  if (!new Events().__proto__) prefix = false;
+}
 
 /**
  * Representation of a single EventEmitter function.
@@ -18,6 +35,7 @@ var prefix = typeof Object.create !== 'function' ? '~' : false;
  * @param {Function} fn Event handler to be called.
  * @param {Mixed} context Context for function execution.
  * @param {Boolean} [once=false] Only emit once
+ * @constructor
  * @api private
  */
 function EE(fn, context, once) {
@@ -34,7 +52,7 @@ function EE(fn, context, once) {
  * @api public
  */
 function EventEmitter() {
-  this._events = prefix ? {} : Object.create(null);
+  this._events = new Events();
 }
 
 /**
@@ -245,7 +263,7 @@ EventEmitter.prototype.removeListener = function removeListener(event, fn, conte
  */
 EventEmitter.prototype.removeAllListeners = function removeAllListeners(event) {
   if (event) delete this._events[prefix ? prefix + event : event];
-  else this._events = prefix ? {} : Object.create(null);
+  else this._events = new Events();
 
   return this;
 };
