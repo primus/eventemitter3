b68eed846e882d9ff9f255ad05a6bbe34364c87b
Sam Noedel <sam.noedel@gmail.com>
2016-09-27 19:04:48 -0700

    Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+declare module 'eventemitter3' {
+
+  /**
+   * Minimal `EventEmitter` interface that is molded against the Node.js
+   * `EventEmitter` interface.
+   */
+  export class EventEmitter {
+    prefix: string;
+
+    /**
+     * Return an array listing the events for which the emitter has registered
+     * listeners.
+     */
+    eventNames(): Array<string | symbol>;
+
+    /**
+     * Return the listeners registered for a given event.
+     */
+    listeners(event: string | symbol): Array<ListenerFn>;
+    listeners(event: string | symbol, exists: boolean): Array<ListenerFn> | boolean;
+
+    /**
+     * Calls each of the listeners registered for a given event.
+     */
+    emit(event: string | symbol, ...args: Array<any>): boolean;
+
+    /**
+     * Add a listener for a given event.
+     */
+    on(event: string | symbol, fn: ListenerFn, context?: any): EventEmitter;
+    addListener(event: string | symbol, fn: ListenerFn, context?: any): EventEmitter;
+
+    /**
+     * Add a one-time listener for a given event.
+     */
+    once(event: string | symbol, fn: ListenerFn, context?: any): EventEmitter;
+
+    /**
+     * Remove the listeners of a given event.
+     */
+    removeListener(event: string | symbol, fn?: ListenerFn, context?: any, once?: boolean): EventEmitter;
+    off(event: string | symbol, fn?: ListenerFn, context?: any, once?: boolean): EventEmitter;
+
+    /**
+     * Remove all listeners, or those of the specified event.
+     */
+    removeAllListeners(event: string | symbol): EventEmitter;
+  }
+}
+
+type ListenerFn = (...args: Array<any>) => void;
