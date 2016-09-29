365bef6c917d319cac858eaf0c01f5e1cdb6cd2a
Sam Noedel <sam.noedel@gmail.com>
2016-09-29 05:39:52 -0700

    Put most specific overload first in typings

--- index.d.ts
+++ index.d.ts
@@ -18,8 +18,8 @@ export class EventEmitter {
   /**
    * Return the listeners registered for a given event.
    */
-  listeners(event: string | symbol): Array<ListenerFn>;
   listeners(event: string | symbol, exists: boolean): Array<ListenerFn> | boolean;
+  listeners(event: string | symbol): Array<ListenerFn>;
 
   /**
    * Calls each of the listeners registered for a given event.
