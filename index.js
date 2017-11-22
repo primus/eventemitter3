e570a19e0055e0f782fd0f765201c4bf178d8235
Luigi Pinca <luigipinca@gmail.com>
2017-11-22 18:10:01 +0100

    [major] Throw an error if the listener argument is not a function
    Throw an error if the second argument of `EventEmitter.prototype.on()`
    and `EventEmitter.prototype.once()` is not a function.
    
    Fixes #78

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ function EE(fn, context, once) {
  * @private
  */
 function addListener(emitter, event, fn, context, once) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('The listener must be a function');
+  }
+
   var listener = new EE(fn, context || emitter, once)
     , evt = prefix ? prefix + event : event;
 
