986bce37c7d3332c4fcb301adff55512c6562237
Emily Marigold Klassen <forivall@gmail.com>
2020-08-24 12:40:44 -0700

    fix: reduce type constraints

--- index.d.ts
+++ index.d.ts
@@ -91,29 +91,41 @@ declare namespace EventEmitter {
     >(): EventEmitter<EventTypes, Context>;
   }
 
-  export type ValidEventTypes =
-    | string
-    | symbol
-    | { [K in string | symbol]: any[] | ((...args: any[]) => void) | undefined };
+  /**
+   * `object` should be in either of the following forms:
+   * ```
+   * interface EventTypes {
+   *   'event-with-parameters': any[]
+   *   'event-with-example-handler': (...args: any[]) => void
+   * }
+   * ```
+   */
+  export type ValidEventTypes = string | symbol | object;
 
   export type EventNames<T extends ValidEventTypes> = T extends string | symbol
     ? T
     : keyof T;
 
   export type ArgumentMap<T extends object> = {
-    [K in keyof T]: T[K] extends (...args: any[]) => void
-      ? Parameters<T[K]>
-      : T[K] extends any[]
-      ? T[K]
-      : any[];
+    [K in keyof T]: ExtractArguments<T[K]>;
   };
+
+  export type ExtractArguments<T> = T extends (...args: any[]) => void
+    ? Parameters<T>
+    : T extends any[]
+    ? T
+    : any[];
 
   export type EventListener<
     T extends ValidEventTypes,
     K extends EventNames<T>
   > = T extends string | symbol
     ? (...args: any[]) => void
-    : (...args: ArgumentMap<Exclude<T, string | symbol>>[K]) => void;
+    : (
+      ...args: ExtractArguments<
+        Exclude<T, string | symbol>[Extract<K, keyof T>]
+      >
+    ) => void;
 
   export type EventArgs<
     T extends ValidEventTypes,
